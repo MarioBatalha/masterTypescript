2241f4ad8026c8404fa6219fd92a58124b88e36b
mariobatalha <mariobatalha9@gmail.com>
2021-03-23 13:11:35 +0100

    iterator

--- web/mosh/index.js
+++ web/mosh/index.js
@@ -19,14 +19,14 @@ var message;
 message = "abc";
 var endsWithC = message.endsWith("c");
 var Point = /** @class */ (function () {
-    function Point() {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
     }
     Point.prototype.draw = function () {
         console.log("X: " + this.x + " , Y: " + this.y);
     };
     return Point;
 }());
-var point = new Point();
-point.x = 1;
-point.y = 2;
+var point = new Point(1, 3);
 point.draw();
--- web/mosh/index.ts
+++ web/mosh/index.ts
@@ -22,21 +22,29 @@ message = "abc";
 let endsWithC = (<string>message).endsWith("c");
 
 //interface
-interface Point {
-  x: number;
-  y: number;
-}
+/*interface Point {
+ private x?: number;
+ private y?: number;
+}*/
 
 class Point {
-  x: number;
-  y: number;
+  constructor(private _x?: number, private _y?: number) {
+    this.x = x;
+    this.y = y;
+  }
+  get x() {
+    return this.x;
+  }
+  set x(value) {
+    if (value < 0) throw new Error("value cannot be less than 0");
+    this._x = value;
+  }
 
   draw() {
     console.log("X: " + this.x + " , Y: " + this.y);
   }
 }
 
-let point = new Point();
-point.x = 1;
-point.y = 2;
+let point = new Point(1, 3);
+
 point.draw();
